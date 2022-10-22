3495ef412bd2d9ebbd34b1e6f1a8b4f68e7eaee8
KaznacheevA.S <kaznacheev.a.s@yandex.ru>
2022-10-22 10:32:30 +0400

    IBAction is  Outlet

--- appColorVix/Controllers/SettingRGBViewController.swift
+++ appColorVix/Controllers/SettingRGBViewController.swift
@@ -9,9 +9,26 @@ import UIKit
 
 class SettingRGBViewController: UIViewController {
 
+    @IBOutlet weak var numberRedLabel: UILabel!
+    @IBOutlet weak var numberGreenLabel: UILabel!
+    @IBOutlet weak var numberBlueLabel: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    @IBOutlet weak var poleRedTF: UITextField!
+    @IBOutlet weak var poleGreenTF: UITextField!
+    @IBOutlet weak var poleBlueTF: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
-
+    
+    // MARK: Dismiss
+    @IBAction func didDoneButton(_ sender: Any) {
+        dismiss(animated: true)
+    }
+    
 }
